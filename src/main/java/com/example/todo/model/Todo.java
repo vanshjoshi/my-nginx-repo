1c355d25e670c70f94284c863f5d6fcdd552093b
vanshjoshi <131679742+vanshjoshi@users.noreply.github.com>
2026-02-03 15:16:02 +0530

    Create Todo.java

---
+++ src/main/java/com/example/todo/model/Todo.java
@@ -0,0 +1,30 @@
+package com.example.todo.model;
+
+public class Todo {
+
+    private Long id;
+    private String title;
+
+    public Todo() {}
+
+    public Todo(Long id, String title) {
+        this.id = id;
+        this.title = title;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
